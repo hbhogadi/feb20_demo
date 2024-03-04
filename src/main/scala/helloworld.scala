71b194d415ca9e40581f94ed3799617f05d63c36
hanishbhogadi <hbhogadi@kent.edu>
2024-03-04 08:45:13 -0600

    hello world program

--- src/main/scala/helloworld.scala
+++ src/main/scala/helloworld.scala
@@ -3,3 +3,15 @@ def main(args:Array[String]):Unit={
   println("Hello_World")
 }
 }
+var i = 1;
+for (int i=1;i<=100;i++)
+{
+  prrintln(i)
+}
+
+var i = 1;
+while (i=100)
+{
+  println(i)
+  i=i+1
+}
